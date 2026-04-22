da5ef8c4b1f8208df6b56663200ce506d3632a5d
Armonia <103010482+leacont@users.noreply.github.com>
2026-04-22 15:22:51 -0300

    Add files via upload

--- planning_drag_generic_19/__manifest__.py
+++ planning_drag_generic_19/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Manufacturing Planning Drag (Generic)",
-    "version": "19.0.1.44.0",
+    "version": "19.0.1.46.0",
     "summary": "Drag & drop production sequencer with real-time work center status, Rush priority and automatic scheduling.",
     "category": "Manufacturing/Manufacturing",
     "author": "Armonia",
